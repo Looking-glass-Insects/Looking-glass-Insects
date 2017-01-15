9e51c8e9d8d09ef506792dd9a5aa04fbe89d6091
Looking-glass-Insects <1070330210@qq.com>
2017-01-15 14:02:09 +0800

    1.15

---
+++ single.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class single:
+    def __init__(self,dimen):
+        self.weight = np.zeros(dimen)
+        self.bias = 0
+        self.ratio = 0.1
+
+    def out(self,inputVec):
+        return sum(self.weight * inputVec) + self.bias
+
+    def train(self,inputSet,outputSet,times = 100):
+        for x in range(0,times):
+            for i in range(0,len(inputSet)):
+                out = self.out(inputSet[i])
+                delta = outputSet[i] - out
+                self.weight += self.ratio * delta * inputSet[i]
+                self.bias += self.ratio * delta
+
+if __name__ == '__main__':
+    single = single(2)
+
+    inputSet = np.array([1,1,2,2]).reshape(2,-1)
+    outputSet = np.array([1,2])
+
+    single.train(inputSet,outputSet)
+
+    print(single.out([1,1]))
