8a18f0f6b226e35da637e30b145f6e95a78f7465
Looking-glass-Insects <1070330210@qq.com>
2017-02-11 18:36:45 +0800

    library

--- app/src/main/java/com/example/heyong/library/MainActivity.java
+++ app/src/main/java/com/example/heyong/library/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.heyong.library;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -27,43 +28,14 @@ public class MainActivity extends SwipeBackActivity {
         rc.setEmptyView(emptyView);
 
 
+        //addCustomNotification();
+        addSimpleNotification();
 
+
+    }
+
+    private void addSimpleNotification() {
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NotificationHelper helper = new NotificationHelper(MainActivity.this);
-                helper.sendCustomNotification(new NotificationHelper.CallBack() {
-                    @Override
-                    public int getLayoutRes() {
-                        return R.layout.empty_view;
-                    }
-
-                    @Override
-                    public int getSmallIconRes() {
-                        return R.mipmap.ic_launcher;
-                    }
-
-                    @Override
-                    public int getId() {
-                        return 0;
-                    }
-
-                    @Override
-                    public void onGetRemoteViews(RemoteViews remoteViews) {
-                        remoteViews.setTextViewText(R.id.tv,"hello world");
-                        remoteViews.setImageViewResource(R.id.iv,R.mipmap.uuz_200px);
-                        remoteViews.setOnClickPendingIntent(R.id.iv,
-                                PendingIntent.getActivity(MainActivity.this,0,
-                                    new Intent(MainActivity.this,MainActivity.class),
-                                    PendingIntent.FLAG_UPDATE_CURRENT
-                                ));
-                    }
-                });
-            }
-        });
-
-
-        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 NotificationHelper helper = new NotificationHelper(MainActivity.this);
@@ -91,6 +63,51 @@ public class MainActivity extends SwipeBackActivity {
                     @Override
                     public int getId() {
                         return 1;
+                    }
+
+                    @Override
+                    public void beforeNotificationBuild(NotificationCompat.Builder builder) {
+                        builder.setAutoCancel(true);
+                    }
+                });
+            }
+        });
+    }
+
+    private void addCustomNotification() {
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationHelper helper = new NotificationHelper(MainActivity.this);
+                helper.sendCustomNotification(new NotificationHelper.CallBack() {
+                    @Override
+                    public int getLayoutRes() {
+                        return R.layout.empty_view;
+                    }
+
+                    @Override
+                    public int getSmallIconRes() {
+                        return R.mipmap.ic_launcher;
+                    }
+
+                    @Override
+                    public int getId() {
+                        return 0;
+                    }
+
+                    @Override
+                    public void onGetRemoteViews(RemoteViews remoteViews) {
+                        remoteViews.setTextViewText(R.id.tv,"hello");
+                        remoteViews.setOnClickPendingIntent(R.id.iv, PendingIntent.getActivity(
+                                MainActivity.this,
+                                0,
+                                new Intent(MainActivity.this,MainActivity.class),
+                                PendingIntent.FLAG_UPDATE_CURRENT));
+                    }
+
+                    @Override
+                    public void beforeNotificationBuild(NotificationCompat.Builder builder) {
+                        builder.setOngoing(true);
                     }
                 });
             }
--- app/src/main/java/com/example/heyong/library/tools/NotificationHelper.java
+++ app/src/main/java/com/example/heyong/library/tools/NotificationHelper.java
@@ -1,6 +1,5 @@
 package com.example.heyong.library.tools;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -18,6 +17,9 @@ public class NotificationHelper {
         manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
+
+
+    @Deprecated
     public void sendSimpleNotification(int smallIconRes, String contentTitle, String contentText, PendingIntent contentIntent, int id) {
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(context)
@@ -28,46 +30,56 @@ public class NotificationHelper {
         manager.notify(id, builder.build());
     }
 
-    public void sendSimpleNotification(SimpleCallBack callBack) {
-        this.sendSimpleNotification(
-                callBack.getSmallIconRes(),
-                callBack.getContentTitle(),
-                callBack.getContentText(),
-                callBack.getPendingIntent(),
-                callBack.getId());
-    }
 
+
+    public void sendSimpleNotification(SimpleCallBack callBack) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(callBack.getSmallIconRes())
+                .setContentTitle(callBack.getContentTitle())
+                .setContentText(callBack.getContentText())
+                .setContentIntent(callBack.getPendingIntent());
+        callBack.beforeNotificationBuild(builder);
+        manager.notify(callBack.getId(), builder.build());
+    }
 
     public void sendCustomNotification(CallBack callBack) {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), callBack.getLayoutRes());
         callBack.onGetRemoteViews(remoteViews);
-        Notification notification = new NotificationCompat.Builder(context)
-                .setSmallIcon(callBack.getSmallIconRes())
-                .setContent(remoteViews).build();
-        manager.notify(callBack.getId(), notification);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setSmallIcon(callBack.getSmallIconRes())
+                .setContent(remoteViews);
+        callBack.beforeNotificationBuild(builder);
+        manager.notify(callBack.getId(), builder.build());
     }
 
-
-    public interface CallBack {
-        int getLayoutRes();
-
-        int getSmallIconRes();
-
-        int getId();
-
-        void onGetRemoteViews(RemoteViews remoteViews);
+    public NotificationManager getManager() {
+        return manager;
     }
 
-    public interface SimpleCallBack {
-        String getContentTitle();
+    static public abstract class CallBack {
+        public abstract int getLayoutRes();
 
-        String getContentText();
+        public abstract int getSmallIconRes();
 
-        int getSmallIconRes();
+        public abstract int getId();
 
-        PendingIntent getPendingIntent();
+        public abstract void onGetRemoteViews(RemoteViews remoteViews);
 
-        int getId();
+        public void beforeNotificationBuild(NotificationCompat.Builder builder){}
+    }
+
+    static public abstract class SimpleCallBack {
+        public abstract String getContentTitle();
+
+        public abstract String getContentText();
+
+        public abstract int getSmallIconRes();
+
+        public abstract PendingIntent getPendingIntent();
+
+        public abstract int getId();
+
+        public void beforeNotificationBuild(NotificationCompat.Builder builder){}
     }
 
 }
